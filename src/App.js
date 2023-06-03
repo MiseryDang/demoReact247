746b6af919d5974c847ab1415acd2284f60e49a0
Dang Dinh Cuong <dangdinhcuong2002@gmail.com>
2023-06-03 08:14:30 +0700

    EX6

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Player from './Players';
+import footer from './component/footer';
+import Navigation from './component/navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Player/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/navigation.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+export default class Navigation extends Component {
+    render() {
+        return (
+            <div className='navigation'>
+                <nav>
+                    <ul>
+                        <li><a className='active' href='#home'>Home</a></li>
+                        <li><a href='#news'>News</a></li>
+                        <li><a href='#abouts'>Abouts</a></li>
+                        <li><a href='#contact'>Contact</a></li>
+
+                    </ul>
+                </nav>
+
+            </div>
+        )
+    }
+
+}
+
