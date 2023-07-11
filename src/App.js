5fd42739f9252b2f5da1db22be9a5095d3430d8c
Dang Dinh Cuong <dangdinhcuong2002@gmail.com>
2023-07-11 10:06:25 +0700

    ex14

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Players from './Players';
 import Main from './components/Main';
 import Footer  from './components/Footer';
 import Detail from './components/Detail';
+import About from './components/About';
+import Contact from './components/Contact';
+
 //import PlayersPresentation from './components/PlayersPresentetion';
 import{Routes,Route} from 'react-router-dom';
 function App(){
@@ -16,9 +19,10 @@ function App(){
           <Route path='/' element={<Players/>}> 
       </Route>
         <Route path='/detail/:id' element={<Detail/>}></Route>
-        {/* <Route path='/contact' element={<Contact/>}></Route> */}
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
       </Routes>
-      <Footer/>
+      <Footer></Footer>
     </div>
 
   );
