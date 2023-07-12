13d0bbe86e5809b89afbb3f3b31f2113562c2b57
Dang Dinh Cuong <dangdinhcuong2002@gmail.com>
2023-07-12 09:57:55 +0700

    pe

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Contact from './components/Contact';
-import Add from './components/Add';
-import {Route, Routes} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Contact from './pages/Contact';
+import FormAddEdit from "./components/FormAddEdit";
+
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Navbar />
+      <ToastContainer position="top-right" autoClose={2000} />
       <Routes>
-        <Route path='/' element={<Contact></Contact>}></Route>
-        <Route path='/add' element={<Add/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/add" element={<FormAddEdit />}></Route>
+        <Route path="/update/:id" element={<FormAddEdit />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
       </Routes>
-    </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
